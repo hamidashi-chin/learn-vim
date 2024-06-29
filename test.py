af7535abbe848edc0831888c2f9f34ddc397ec7c
hamidashi-chin <take106141@gmail.com>
2024-06-29 20:38:15 +0900

    Visualモードでインデントとかコピーとか

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
 import sys
+import os
 
 def test_func():
     print('test hello world!!! Good')
     print('test hello world!!! Good')
-    print('test hello world!!! Good')
-    print('test hello world!!! Good')
+    print('apple hello world!!! Rood')
+    print('applexxxxxxxxxxxxxxxGood')
 
 def test_func():
     print('test hello world!!! Good')
     print('test hello world!!! Good')
     print(sys.maxint)
     print('test hello world!!! Good')
-    print('test hello world!!! Good')
+    print('test hello world!!! Good line number2')
 
 def test_func():
     print('test hello world!!! Good')
