9196af1bc6cdd0acf823d08f6bae3cc2823bd4ae
hamidashi-chin <take106141@gmail.com>
2024-06-29 21:36:04 +0900

    nvimのコンフィグ設定

--- test.py
+++ test.py
@@ -7,12 +7,13 @@ def test_func():
     print('apple hello world!!! Rood')
     print('applexxxxxxxxxxxxxxxGood')
 
-def test_func():
-    print('test hello world!!! Good')
-    print('test hello world!!! Good')
-    print(sys.maxint)
-    print('test hello world!!! Good')
-    print('test hello world!!! Good line number2')
+    def test_func():
+        # print('test hello world!!! Good') // comment
+        # print('test hello world!!! Good') // comment
+        # print(sys.maxint) // comment
+        # print('test hello world!!! Good') // comment
+        # print('test hello world!!! Good line number2') // comment
+        # print('test_test_test_test_test_test_test_test_test_') // comment
 
 def test_func():
     print('test hello world!!! Good')
