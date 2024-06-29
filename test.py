3c4e3b07094be8344fa8694b64fe141c99f6802b
hamidashi-chin <take106141@gmail.com>
2024-06-29 17:59:02 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,22 @@
+import sys
+
+def test_func():
+    print('test hello world!!! Good')
+    print('test hello world!!! Good')
+    print('test hello world!!! Good')
+    print('test hello world!!! Good')
+
+def test_func():
+    print('test hello world!!! Good')
+    print('test hello world!!! Good')
+    print(sys.maxint)
+    print('test hello world!!! Good')
+    print('test hello world!!! Good')
+
+def test_func():
+    print('test hello world!!! Good')
+    print('test hello world!!! Good')
+    print('test hello world!!! Good')
+    print('test hello world!!! Good')
+
+test_func()
